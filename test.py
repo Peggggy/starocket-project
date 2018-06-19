8d6b7f012c2c290868d36f074aae20f395053360
Peggggy <smallereyes@gmail.com>
2018-06-19 20:59:25 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+n1=input("Enter 1st number:")
+n2=input("Enter 2nd number:")
+n3=input("Enter 3rd number:")
+n4=input("Enter 4th number:")
+n5=input("Enter 5th number:")
+
+n1= int(n1)
+n2= int(n2)
+n3= int(n3)
+n4= int(n4)
+n5= int(n5)
+
+data=[n1,n2,n3,n4,n5]
+print(sum(data))
+print(max(data))
+
